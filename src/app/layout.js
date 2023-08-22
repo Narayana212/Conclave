cc93f2c17d434de1af9f656136e5c1e97a818415
adikal25 <ka584@@snu.edu.in>
2023-08-22 17:52:22 +0530

    adding components structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Navbar from '../components/navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
@@ -11,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      
+      <Navbar></Navbar>
       {children}
       
       </body>
---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar () {
+  return (
+      <div className="w-screen h-auto display-flex">
+          Navbar
+    </div>
+  )
+}
