ee8c726479cf7b4108b2efe52ef9f5e6461de856
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-10-10 12:12:31 +0530

    middleware functionality

--- src/app/api/logout/route.js
+++ src/app/api/logout/route.js
@@ -1,13 +1,18 @@
-import { NextResponse } from "next/server";
+const { NextResponse } = require("next/server");
 
-export async function GET(){
-    try{
-        const response=NextResponse.json({message:"Logout successful"},{status:200})
-        response.cookies.set("token","")
-        return response
-    }catch(err){
-        throw new Error(err.message)
-       
+async function GET() {
+    try {
+        const response = NextResponse.json(
+            {
+                message: "Logout successful",
+                success: true,
+            }
+        );
+        response.cookies.set("token", "", 
+        { httpOnly: true, expires: new Date(0) 
+        });
+        return response;
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
-
 }
