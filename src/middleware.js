cb1a8e6b92be820151f25c8d39b528ecf92cf3f0
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-10-10 11:16:18 +0530

    middleware functionality

--- src/middleware.js
+++ src/middleware.js
@@ -1,16 +1,16 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-    /*const path=request.nextUrl.pathname
+    const path=request.nextUrl.pathname
     const isPublicPath= path==='/login' || path==='/signup'
     const token = request.cookies.get('token')|| ""
-    if(!isPublicPath && !token.value){
-        return NextResponse.redirect(new URL("/login",request.nextUrl))
-    }*/
+    if(isPublicPath && token.value){
+        return NextResponse.redirect(new URL("/",request.nextUrl))
+    }
   
 }
 
 
 export const config = {
-  matcher: ["/login", "/", "/signup","/success",'/contact','/events'],
+  matcher: ["/login", "/", "/signup","/success",'/contact','/events',"/tickets"],
 };
