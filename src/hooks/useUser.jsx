0b6accc06a916f54386bb258e6446904659857ce
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-10-11 13:27:44 +0530

    Completed all login functionality and nessicitie

--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -11,7 +11,7 @@ export default function useUser() {
       const data = await response.json();
 
       if (response.ok) {
-        console.log(data)
+        console.log("useUser",data)
         setUsername(data.message.fullName);
       } else {
         setUsername("");
