6aaab7f329aa2d1904fb268da43174d31ada7296
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-10-11 20:55:00 +0530

    Completed all login functionality and nessicitie

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -6,11 +6,14 @@ export const getDataFromToken = async (request: NextRequest) => {
       const secret = new TextEncoder().encode(
         'cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2',
       )
-        const token = request.cookies.get("token")?.value || '';
-        console.log(token);
-        const verified = await jose.jwtVerify(token, secret)
+      const token = request.cookies.get("token");
+      if(token){
+        const verified = await jose.jwtVerify(token.value, secret)
         const decodedToken=verified.payload
         return decodedToken ;
+      }else{
+        return {email:token,fullName:token.value+"@"}
+      }
     } catch (error: any) {
       console.log(error.message)
       return {email:"",fullName:error.message};
