8a2846f4d4348f436cc0845adb92d511192244dd
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-10-11 12:44:29 +0530

    Completed all login functionality and nessicitie

--- src/app/api/username/route.js
+++ src/app/api/username/route.js
@@ -1,12 +1,12 @@
 import { getDataFromToken } from "../../../helpers/getDataFromToken";
 import { NextResponse } from "next/server";
-import prisma from "../../../lib/prisma";
 
 
 export async function GET(request) {
   try {
     const decodedToken = getDataFromToken(request);
     const { email,fullName } = decodedToken;
+    console.log(decodedToken)
     return NextResponse.json({ message: { email, fullName } },{status:200});
   } catch (err) {
     console.log(err.message)
---
+++ src/helpers/getDataFromToken.ts
@@ -0,0 +1,17 @@
+import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+
+export const getDataFromToken = (request: NextRequest) => {
+    try {
+        const token = request.cookies.get("token")?.value || '';
+        console.log(token);
+        const decodedToken:{email:string,fullName:string} = jwt.verify(token, "secret");
+        return decodedToken;
+    } catch (error: any) {
+      console.log(error.message)
+      return {email: "",fullName: ""};
+      
+        
+    }
+
+}
