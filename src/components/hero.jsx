15563e2de1d9a2921c7affcf4311396a3c4bacd5
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-10-15 21:31:45 +0530

    pics and content

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -6,6 +6,7 @@ import Navbar from "./navbar";
 import { Button } from "./ui/button";
 
 import Link from "next/link";
+import { Calendar, CalendarCheck, CalendarDays, GitPullRequest, LocateFixed, LocateIcon, LocateOffIcon, MapPin, Pin, PinOff } from "lucide-react";
 
 export default function Hero({ isLogin }) {
   const containerVariants = {
@@ -42,15 +43,17 @@ export default function Hero({ isLogin }) {
         <div className="w-full h-full opacity-[0.85] bg-[#290E13]">
           <Navbar isLogin={isLogin} />
           <motion.div
-            className="flex md:hidden flex-col items-center justify-center w-full h-full "
+            className="flex md:hidden flex-col items-center justify-center -mt-10 w-full h-full "
             variants={textVariants}
           >
             <Heading text={"BUSINESS"} styles="text-5xl xs:text-6xl " />
             <Heading text={"CONCLAVE"} styles="text-5xl xs:text-6xl" />
             <Heading text={"2023"} styles="text-5xl xs:text-6xl " />
+            <p className="text-white flex  items-center gap-2 mt-2"><CalendarDays/>28-10-2023</p>
+            <p className="text-white flex  items-center gap-2 mt-2"><MapPin/>SNU Nodia Campus</p>
             <Link href="/tickets">
               <motion.div
-                className={` ${isLogin ? "flex" : "hidden"}  mt-10 `}
+                className={` ${isLogin ? "flex" : "hidden"}  mt-5 `}
                 variants={buttonVariants}
               >
                 <Button>GET TICKETS</Button>
@@ -64,20 +67,24 @@ export default function Hero({ isLogin }) {
             <div className="flex flex-col items-start">
               <Heading text={"BUSINESS"} styles="md:text-8xl lg:text-9xl" />
               <Heading text={"CONCLAVE"} styles="md:text-8xl lg:text-9xl" />
+              <p className="text-white flex self-end items-center gap-2 mt-2"><CalendarDays/>28-10-2023</p>
+            <p className="text-white flex self-end items-start gap-2 mt-2"><MapPin/>SNU Nodia Campus</p>
               <Link href="/tickets" className="self-center -mr-[8rem]">
+
                 <motion.div
-                  className={`${isLogin ? "" : "hidden"}  mt-10`}
+                  className={`${isLogin ? "" : "hidden"}  mt-5`}
                   variants={buttonVariants}
                 >
                   <Button>GET TICKETS</Button>
                 </motion.div>
+
               </Link>
             </div>
             <motion.div variants={textVariants}>
               <Heading
                 text={"2023"}
-                styles={` ${isLogin ? "-mt-[4rem]" : "-mt-[1rem]"
-                  } md:text-7xl  lg:text-8xl -ml-12 rotate-90`}
+                styles={` ${isLogin ? "-mt-[7rem]" : "-mt-[4.5rem]"
+                  } md:text-7xl  lg:text-8xl -ml-14 rotate-90`}
 
               />
             </motion.div>
