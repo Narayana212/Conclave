9514a3821dd6f215d9a48403902ba28da0360c0a
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-10-15 11:26:27 +0530

    pics and content

--- src/app/(root)/(routes)/tickets/page.jsx
+++ src/app/(root)/(routes)/tickets/page.jsx
@@ -154,7 +154,7 @@ export default function TicketPage() {
           )}
         </div>
       ) : (
-        <Tabs defaultValue="account" className="w-[400px]  mx-auto">
+        <Tabs defaultValue="account" className="w-[400px] pl-5 ">
           <TabsList>
             <TabsTrigger value="account">For SNU Students</TabsTrigger>
             <TabsTrigger value="password">For Non-SNU Students</TabsTrigger>
