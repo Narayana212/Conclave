fb576866b0d052c0315cf62ed69d4b7c5d8aedfb
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-10-15 21:40:58 +0530

    pics and content

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -6,7 +6,18 @@ import Navbar from "./navbar";
 import { Button } from "./ui/button";
 
 import Link from "next/link";
-import { Calendar, CalendarCheck, CalendarDays, GitPullRequest, LocateFixed, LocateIcon, LocateOffIcon, MapPin, Pin, PinOff } from "lucide-react";
+import {
+  Calendar,
+  CalendarCheck,
+  CalendarDays,
+  GitPullRequest,
+  LocateFixed,
+  LocateIcon,
+  LocateOffIcon,
+  MapPin,
+  Pin,
+  PinOff,
+} from "lucide-react";
 
 export default function Hero({ isLogin }) {
   const containerVariants = {
@@ -49,8 +60,31 @@ export default function Hero({ isLogin }) {
             <Heading text={"BUSINESS"} styles="text-5xl xs:text-6xl " />
             <Heading text={"CONCLAVE"} styles="text-5xl xs:text-6xl" />
             <Heading text={"2023"} styles="text-5xl xs:text-6xl " />
-            <p className="text-white flex  items-center gap-2 mt-2"><CalendarDays/>28-10-2023</p>
-            <p className="text-white flex  items-center gap-2 mt-2"><MapPin/>SNIoE, Delhi NCR</p>
+            <div className="flex gap-4 self-center "> <p
+                style={{
+                  background:
+                    "linear-gradient(161deg, #AC016A -11.7%, #FBA150 101.48%)",
+                  backgroundClip: "text",
+                  WebkitBackgroundClip: "text",
+                  WebkitTextFillColor: "transparent",
+                }}
+                className="font-bold text-sm flex self-center items-center gap-2 mt-2"
+              >
+                <CalendarDays
+                  className="text-white  w-5 h-5"
+                />
+                28-10-2023
+              </p>
+              <p style={{
+                  background:
+                    "linear-gradient(161deg, #AC016A -11.7%, #FBA150 101.48%)",
+                  backgroundClip: "text",
+                  WebkitBackgroundClip: "text",
+                  WebkitTextFillColor: "transparent",
+                }} className=" font-bold  text-sm flex self-end items-center gap-2 mt-2">
+                <MapPin className="text-white w-5 h-5"  />
+                SNIoE, Delhi NCR
+              </p></div>
             <Link href="/tickets">
               <motion.div
                 className={` ${isLogin ? "flex" : "hidden"}  mt-5 `}
@@ -67,25 +101,46 @@ export default function Hero({ isLogin }) {
             <div className="flex flex-col items-start">
               <Heading text={"BUSINESS"} styles="md:text-8xl lg:text-9xl" />
               <Heading text={"CONCLAVE"} styles="md:text-8xl lg:text-9xl" />
-              <p className="text-white flex self-end items-center gap-2 mt-2"><CalendarDays/>28-10-2023</p>
-            <p className="text-white flex self-end items-start gap-2 mt-2"><MapPin/>SNIoE, Delhi NCR</p>
+             <div className="flex gap-4 self-center ml-20"> <p
+                style={{
+                  background:
+                    "linear-gradient(161deg, #AC016A -11.7%, #FBA150 101.48%)",
+                  backgroundClip: "text",
+                  WebkitBackgroundClip: "text",
+                  WebkitTextFillColor: "transparent",
+                }}
+                className="font-bold flex self-center items-center gap-2 mt-2"
+              >
+                <CalendarDays
+                  className="text-white"
+                />
+                28-10-2023
+              </p>
+              <p style={{
+                  background:
+                    "linear-gradient(161deg, #AC016A -11.7%, #FBA150 101.48%)",
+                  backgroundClip: "text",
+                  WebkitBackgroundClip: "text",
+                  WebkitTextFillColor: "transparent",
+                }} className=" font-bold flex self-end items-start gap-2 mt-2">
+                <MapPin className="text-white"  />
+                SNIoE, Delhi NCR
+              </p></div>
               <Link href="/tickets" className="self-center -mr-[8rem]">
-
                 <motion.div
                   className={`${isLogin ? "" : "hidden"}  mt-5`}
                   variants={buttonVariants}
                 >
                   <Button>GET TICKETS</Button>
                 </motion.div>
-
               </Link>
             </div>
             <motion.div variants={textVariants}>
               <Heading
                 text={"2023"}
-                styles={` ${isLogin ? "-mt-[7rem]" : "-mt-[4.5rem]"
-                  } md:text-7xl  lg:text-8xl -ml-14 rotate-90`}
-
+                styles={` ${
+                  isLogin ? "-mt-[5rem]" : "-mt-[4.5rem]"
+                } md:text-7xl  lg:text-8xl -ml-14 rotate-90`}
               />
             </motion.div>
           </motion.div>
